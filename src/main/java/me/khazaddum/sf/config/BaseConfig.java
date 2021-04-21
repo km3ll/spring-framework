c9f476273f10faca4d89e40b70089d67f9c22cda
km3ll <kmell@protonmail.com>
2021-04-20 21:55:16 -0500

    Add notes and code examples

--- src/main/java/me/khazaddum/sf/config/BaseConfig.java
+++ src/main/java/me/khazaddum/sf/config/BaseConfig.java
@@ -1,11 +1,10 @@
 package me.khazaddum.sf.config;
 
-import me.khazaddum.sf.infrastructure.BeanNoAnnotations;
+import me.khazaddum.sf.infrastructure.component.BeanNoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /* Mix and run multiple configurations: @Import({EmailConfig.class}) */
 @Configuration
