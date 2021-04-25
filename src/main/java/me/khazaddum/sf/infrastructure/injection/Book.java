35a4617ad94738f50c8c5ceb9056d98bbeb93d31
km3ll <kmell@protonmail.com>
2021-04-25 14:21:35 -0500

    Fix DI issue in BookServices
    Those services depend on a Book Bean that was not wired to Spring context.

--- src/main/java/me/khazaddum/sf/infrastructure/injection/Book.java
+++ src/main/java/me/khazaddum/sf/infrastructure/injection/Book.java
@@ -1,4 +1,17 @@
 package me.khazaddum.sf.infrastructure.injection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Book {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Book.class);
+
+    public Book() {
+        super();
+        LOG.info("Created");
+    }
+
 }
