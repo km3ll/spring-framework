12c9b7d8044c935f2c14eb454a55d27025a65183
km3ll <kmell@protonmail.com>
2021-05-13 21:00:49 -0500

    Add examples with logging configurations

---
+++ src/main/java/me/khazaddum/sf/infrastructure/logging/LoggingModuleA.java
@@ -0,0 +1,25 @@
+package me.khazaddum.sf.infrastructure.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Profile("dev")
+public class LoggingModuleA {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingModuleA.class);
+
+    public LoggingModuleA() {
+        LOG.debug("Created");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        LOG.debug("Post construct called");
+    }
+
+}
