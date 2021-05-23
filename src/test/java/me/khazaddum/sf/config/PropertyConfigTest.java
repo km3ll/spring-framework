82c634ef26cf740c76a06711b0e14ddd3d59d201
km3ll <kmell@protonmail.com>
2021-05-22 22:40:30 -0500

    Add examples with Properties source files

---
+++ src/test/java/me/khazaddum/sf/config/PropertyConfigTest.java
@@ -0,0 +1,29 @@
+package me.khazaddum.sf.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import me.khazaddum.sf.Application;
+import me.khazaddum.sf.infrastructure.testing.GreetingRepository;
+import me.khazaddum.sf.infrastructure.testing.GreetingServiceI;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+
+@SpringJUnitConfig(value = Application.class)
+@TestPropertySource(locations = {"classpath:application.properties", "classpath:test.properties"})
+public class PropertyConfigTest {
+
+    @Value("${test.property}")
+    private String testProperty;
+
+    @Test
+    public void whenTestPropertySource_thenValueRetrieved() {
+        assertEquals( "testdrive", testProperty );
+    }
+
+}
