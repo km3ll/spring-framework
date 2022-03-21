b56883d16344fb86d94e291dba3be33fa981aea9
km3ll <kmell@protonmail.com>
2022-03-21 16:21:57 -0500

    Add example of parameter scanBasePackageClasses

--- src/main/java/me/khazaddum/spring/m02/M02Application.java
+++ src/main/java/me/khazaddum/spring/m02/M02Application.java
@@ -3,6 +3,7 @@ package me.khazaddum.spring.m02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(scanBasePackageClasses = "me.khazaddum.spring.m02")
 @SpringBootApplication
 public class M02Application {
 
